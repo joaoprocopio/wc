47a2fd66563e7b8f725e47237bd35ec42bf41e66
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-11-12 01:22:08 -0300

    first web component

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
+import { LitElement, html } from "lit"
+import { customElement } from "lit/decorators.js"
+
+@customElement("my-element")
+class MyElement extends LitElement {
+  render() {
+    return html`
+      <div>hello from my element!</div>
+    `
+  }
+}
+
 const rootEl = document.getElementById("__root") as HTMLDivElement
 
 rootEl.innerHTML = `
-  <div>hello world</div>
+  <my-element></my-element>
 `
